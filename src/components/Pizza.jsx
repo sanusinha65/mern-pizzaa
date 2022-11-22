fd28966a74a335b0a2700bbc85c5bd4c4416e031
Aditya Dhanraj <sanusinha65@gmail.com>
2022-11-22 18:21:41 +0530

    Modals Added

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import { Button } from "bootstrap";
+import React, {useState} from "react";
+import {Modal} from 'react-bootstrap';
 
 export default function Pizza({ pizza }) {
+  const [quantity, setquantity]= useState(1);
+  const [varients, setvarients]= useState("small");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>
+    <div className="shadow-lg p-3 mb-5 bg-white rounded" style={{marginLeft: "30px", padding: "1%", borderRadius: "20px", border: "1px solid whitesmoke"}}>
+
+<div onClick={handleShow}>
       <h1>{pizza.name}</h1>
       <img
         src={pizza.image}
@@ -10,13 +21,58 @@ export default function Pizza({ pizza }) {
         className="img-fluid"
         style={{ width: "200px", height: "200px" }}
       />
+      </div>
       <div className="container">
         <div className="w-100">
           <p>Varients</p>
+          <select value={varients} onChange={(e)=>{setvarients(e.target.value)}} style={{ textTransform: "capitalize" }}>
+            {pizza.varients.map((varient) => {
+              return (
+                <option value={varient} style={{ textTransform: "capitalize" }}>
+                  {varient}
+                </option>
+              );
+            })}
+          </select>
+          
         </div>
+        <div>
+          <p> Quantity</p>
+<select value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
+  {[...Array(10).keys()].map((x, i)=>{
+    return (
+      <option value={i+1} style={{ textTransform: "capitalize" }}>
+        {i+1}
+      </option>
+    );
 
-        <div>Prices</div>
+  })}
+        </select>
+        </div> 
+        </div>
+        <div className="container"> 
+        <div>
+        <h1>Prices: {pizza.prices[0][varients] * [quantity]}</h1>
+        </div>
+        <div>
+          <button className="btn btn-success ">Add To Cart</button>
+        </div>
+         </div>
+
+      <Modal show={show}>
+      <Modal.Header closeButton onClick={handleClose}>
+        <Modal.Title>{pizza.name}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body style={{display: "grid", justifyItems: "center"}}>
+        <img src={pizza.image} alt={pizza.name} />
+        <p>{pizza.description}</p>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <button className="btn btn-secondary mt-0" onClick={handleClose}>Close</button>
+      </Modal.Footer>
+    </Modal>
       </div>
-    </div>
   );
 }
--- src/screens/Homescreen.jsx
+++ src/screens/Homescreen.jsx
@@ -4,7 +4,7 @@ import Pizza from '../components/Pizza';
 export default function Homescreen() {
   return (
     <div>
-      <div className="row">
+      <div className="row" style={{width: "100%"}}>
         {pizzas.map(pizza=>{
           return <div className="col-md-4">
             <Pizza pizza={pizza} />
